61df1a6b36578bd7571e5c33e21b49a2fbda1662
SqueeOnE <selih.design@gmail.com>
2022-11-18 10:59:38 +0100

    Add user dropdown to navbar with login and register links

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { useApolloClient } from "@apollo/client";
 import { useRouter } from "next/router";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import NextLink from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Menu, Transition } from "@headlessui/react";
 
 interface NavbarProps {}
 
@@ -11,6 +14,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   const [logout, { loading: logoutFetching }] = useLogoutMutation();
   const apolloClient = useApolloClient();
   const { data, loading } = useMeQuery({});
+  const [visible, setVisible] = useState("invisible");
 
   let body = null;
 
@@ -19,14 +23,69 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     // user not logged in
   } else if (!data?.me) {
     body = (
-      <>
-        <NextLink href="/login">
-          <div className="mr-2">login</div>
-        </NextLink>
-        <NextLink href="/register">
-          <div>login</div>
-        </NextLink>
-      </>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="text-gray-200 hover:text-white">
+            <FontAwesomeIcon icon={faUser} className="w-6 h-6" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-4 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-3 px-6 bg-gray-200 hover:bg-white">
+              <Menu.Item>
+                <NextLink href="/login">
+                  <div className="mr-2">login</div>{" "}
+                </NextLink>
+              </Menu.Item>
+            </div>
+            <div className="py-3 px-6 bg-gray-200 hover:bg-white">
+              <Menu.Item>
+                <NextLink href="/register">
+                  <div>register</div>
+                </NextLink>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+      // <div className="relative">
+      //   <button
+      //     type="button"
+      //     id="menu-button"
+      //     aria-expanded="true"
+      //     aria-haspopup="true"
+      //     onClick={() =>
+      //       visible === "invisible"
+      //         ? setVisible("visible")
+      //         : setVisible("invisible")
+      //     }
+      //   >
+      //     <FontAwesomeIcon icon={faUser} />
+      //   </button>
+      //   <div
+      //     className={`absolute right-0 z-10 ${visible} mt-4 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none py-3 px-6`}
+      //     role="menu"
+      //     aria-orientation="vertical"
+      //     aria-labelledby="menu-button"
+      //   >
+      //     <div className="py-1">
+      //       <NextLink href="/login">
+      //         <div className="mr-2">login</div>
+      //       </NextLink>
+      //       <NextLink href="/register">
+      //         <div>register</div>
+      //       </NextLink>
+      //     </div>
+      //   </div>
+      // </div>
     );
   } else {
     body = (
@@ -43,7 +102,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   }
 
   return (
-    <div className="flex sticky top-0 bg-cyan-900 p-4">
+    <div className="flex justify-end sticky top-0 bg-cyan-900 p-4">
       <div>{body}</div>
     </div>
   );
